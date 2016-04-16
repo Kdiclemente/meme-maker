695710361d8bb14f05a0c4bb82b140c0b0dc2385
Harrison Shoebridge <harrison@theshoebridges.com>
2016-04-16 12:25:29 +1000

    Add meme making factory
    WARNING: FACTORY IS NOT YET PRODUCING ANYTHING

--- bot.go
+++ bot.go
@@ -17,6 +17,7 @@ var (
 	pageToken   = conf.String("page-token", "not skrilla", "The token that is used to verify the page on facebook")
 
 	states map[int64]MessageState
+	memes  map[int64]*Meme
 
 	client *messenger.Messenger
 )
@@ -26,12 +27,17 @@ const (
 	MakingMeme
 )
 
-func main() {
+func init() {
 	conf.Use(configure.NewFlag())
 	conf.Use(configure.NewEnvironment())
 	conf.Use(configure.NewJSONFromFile("config.json"))
+}
 
+func main() {
 	conf.Parse()
+
+	memes = make(map[int64]*Meme)
+	states = make(map[int64]MessageState)
 
 	client = messenger.New(messenger.Options{
 		Verify:      *verify,
@@ -60,9 +66,51 @@ func messages(m messenger.Message, r *messenger.Response) {
 		r.Text(fmt.Sprintf("Greetings, %v? You're here to make a meme?", from.FirstName))
 		r.Text("If so, you are in just the right place.")
 		r.Text("All you need to do is send me a picture and a line of text to put on that picture!")
+
+		setState(m.Sender, MakingMeme)
+	case MakingMeme:
+		meme := messageMeme(m.Sender)
+
+		if len(m.Attachments) > 0 {
+			a := m.Attachments[0]
+			if a.Type != "image" {
+				r.Text("Sorry to be a sad pepe. Unfortunately you're going to need to send an image")
+			}
+
+			meme.ImageURL = a.Payload.URL
+		}
+
+		if m.Text != "" {
+			meme.Text = m.Text
+		}
+
+		fmt.Println(meme.Ready())
 	}
 }
 
 func messageState(s messenger.Sender) MessageState {
 	return states[s.ID]
+}
+
+func setState(s messenger.Sender, state MessageState) {
+	states[s.ID] = state
+}
+
+func messageMeme(s messenger.Sender) *Meme {
+	meme := memes[s.ID]
+	if meme == nil {
+		meme = &Meme{}
+		memes[s.ID] = meme
+	}
+
+	return meme
+}
+
+type Meme struct {
+	ImageURL string
+	Text     string
+}
+
+func (m Meme) Ready() bool {
+	return m.ImageURL != "" && m.Text != ""
 }
